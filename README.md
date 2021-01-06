b6a52546dcf1d62d191925c065a49fc2c2f91d08
Eoin Traynor <eointraynor1@gmail.com>
2021-01-06 23:30:18 +0000

    initial v1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+## My personal NPM package
+
+This NPM package is my virtual business card 💼
+
+Just run:
+```sh
+npx eointraynor
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+console.log(`
+  Eoin Traynor
+  Website - https://www.eointraynor.com
+  Github - https://github.com/EoinTraynor
+  Twitter - https://twitter.com/EoinTraynor1
+  LinkedIn - https://www.linkedin.com/in/eointraynor/
+  Email - eointraynor1@gmail.com
+`);
