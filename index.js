d2ac1ab445ead50c545ae25344632b7c9d7252b6
Eoin Traynor <eointraynor1@gmail.com>
2021-01-14 00:11:30 +0000

    add occupation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 console.log(`
-  Eoin Traynor
-  Website - https://www.eointraynor.com
-  Github - https://github.com/EoinTraynor
-  Twitter - https://twitter.com/EoinTraynor1
-  LinkedIn - https://www.linkedin.com/in/eointraynor/
-  Email - eointraynor1@gmail.com
+  Eoin Traynor 👋
+  Developer @ HubSpot
+
+  Website: https://www.eointraynor.com
+  Github: https://github.com/EoinTraynor
+  Twitter: https://twitter.com/EoinTraynor1
+  LinkedIn: https://www.linkedin.com/in/eointraynor/
+  Email: eointraynor1@gmail.com
 `);
